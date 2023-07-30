431847f5c4ba3076ec32a9e46c47ebc20ed3efc9
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2023-07-30 15:51:37 +0530

    v01.64

--- src/home.jsx
+++ src/home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
             <h1>I,m  Abhimanyu <span>Prajapati</span></h1>
             <p>Full Stack Web Developer</p>
             <div className="download">
-                    <a href="./src/assets/Abhimanyu_Resume.pdf" download ><FileDownloadOutlinedIcon/> Download Resume</a>
+                    <a href="https://raw.githubusercontent.com/Abhimanyuprajapati/Portfolio/844b4e896b7a679f1055dd3ea0e010083a36ace4/src/assets/Abhimanyu_Resume.pdf" download ><FileDownloadOutlinedIcon/> Download Resume</a>
             </div>
             <div className="socialmedia">
                 <a href="https://www.linkedin.com/in/abhimanyu-prajapati-84b677215"><LinkedInIcon/></a>
@@ -26,3 +26,9 @@ export const Home = () => {
     </div>
 );
 }
+
+
+
+/**
+ * <div class="pdf-container loaded" data-file="https://raw.githubusercontent.com/Abhimanyuprajapati/Portfolio/844b4e896b7a679f1055dd3ea0e010083a36ace4/src/assets/Abhimanyu_Resume.pdf" data-bcmap-dir="//viewscreen.githubusercontent.com/static/pdf" data-pdf-worker-js-path="/static/assets/pdf.worker-b0807d516e641f5fedeb.js"><canvas class="pdf-page" data-page="0" height="2588" width="2000"></canvas></div>
+ */
