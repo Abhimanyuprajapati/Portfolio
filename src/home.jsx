844b4e896b7a679f1055dd3ea0e010083a36ace4
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2023-07-30 15:29:23 +0530

    v01.63

--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,8 @@
 import "./home.css";
-
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
 export const Home = () => {
     return (
     <div className="main">
@@ -10,7 +13,16 @@ export const Home = () => {
         <div className="detail">
             <h1>I,m  Abhimanyu <span>Prajapati</span></h1>
             <p>Full Stack Web Developer</p>
+            <div className="download">
+                    <a href="./src/assets/Abhimanyu_Resume.pdf" download ><FileDownloadOutlinedIcon/> Download Resume</a>
+            </div>
+            <div className="socialmedia">
+                <a href="https://www.linkedin.com/in/abhimanyu-prajapati-84b677215"><LinkedInIcon/></a>
+                <a href="https://github.com/Abhimanyuprajapati"><GitHubIcon/></a>
+                <a href="https://www.instagram.com/abhimanyuprajapati77/"><InstagramIcon/></a>
+            </div>  
         </div>
+        
     </div>
 );
 }
