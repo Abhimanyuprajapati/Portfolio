9a924095fa38aac58006bf52d016dd422f3fe339
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2023-07-30 22:16:36 +0530

    new one

--- src/home.jsx
+++ src/home.jsx
@@ -3,6 +3,7 @@ import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
+
 export const Home = () => {
     return (
     <div className="main">
