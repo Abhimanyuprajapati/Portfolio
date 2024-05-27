83893c2ce83b171492097bf4fb3ae1b1db828be2
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2024-05-27 18:56:53 +0530

    1not

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,11 +3,18 @@ import { NavLink } from "react-router-dom";
 export const Navbar = () => {
   return (
     <nav className='navbar'>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-        <NavLink to="skill">Skill</NavLink>
-        <NavLink to="project">Project</NavLink>
-        <NavLink to="contact">Contact</NavLink>
+
+      <div className='navsymbol'>
+        <span><NavLink to="/">@bhimanyu</NavLink></span>
+      </div>
+      <div className='navhead'>
+       <li><NavLink to="/">Home</NavLink></li>
+       <li><NavLink to="about">About</NavLink></li>
+       <li><NavLink to="skill">Skill</NavLink></li>
+       <li><NavLink to="project">Project</NavLink></li>
+       <li><NavLink to="contact">Contact</NavLink></li>
+      </div>
+
     </nav>
   )
 };
